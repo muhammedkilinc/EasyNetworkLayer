0f7da8c569468db03aa0cc15076f281775ab71a2
muhammed kılınç <muhammed.kilinc@inomera.com>
2018-11-06 16:04:00 +0300

    Initial Commit

---
+++ EasyNetworkLayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyNetworkLayer
+//
+//  Created by ınomera on 6.11.2018.
+//  Copyright © 2018 muhammedkilinc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
