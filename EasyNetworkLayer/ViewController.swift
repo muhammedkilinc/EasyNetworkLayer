039a94a3d959046230ee9cf4ec1619b775bbf33a
muhammed kılınç <muhammed.kilinc@inomera.com>
2018-11-07 09:50:58 +0300

    -display fetched data in uitableview

--- EasyNetworkLayer/ViewController.swift
+++ EasyNetworkLayer/ViewController.swift
@@ -10,38 +10,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var dataArray: [Source] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-//        let fetcher = Fetcher()
-//        fetcher.fetch(ofObjectType: SourceResponse.self, atURL: "sources", parameters: ["apiKey": "3073a66586c646cb8ca68c805efdff12" as AnyObject]) { (result) in
-//            switch result {
-//            case .error(let error):
-//                print(error)
-//                break
-//            case .success(result: let result):
-//                if let items = result as? SourceResponse {
-//                   print(items.sources)
-//                }
-//                break
-//            }
-//        }
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
         
         let fetcher = Fetcher()
-        fetcher.fetch(endpointType: NewsFeed.getTopHeadlines(countryId: "tr"), objectType: ArticleResponse.self) { (result) in
+        fetcher.fetch(endpointType: NewsFeed.getSources, objectType: SourceResponse.self) { (result) in
             switch result {
             case .error(let error):
                 print(error)
                 break
             case .success(result: let result):
-                if let items = result as? ArticleResponse {
-                    print(items.articles)
+                if let item = result as? SourceResponse {
+                    print(item.sources)
+                    
+                    self.dataArray = item.sources
+                    
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+
                 }
                 break
             }
         }
+        
+//        let fetcher = Fetcher()
+//        fetcher.fetch(endpointType: NewsFeed.getTopHeadlines(countryId: "tr"), objectType: ArticleResponse.self) { (result) in
+//            switch result {
+//            case .error(let error):
+//                print(error)
+//                break
+//            case .success(result: let result):
+//                if let item = result as? ArticleResponse {
+//                    print(item.articles)
+//                    self.dataArray = item.articles
+//
+//                    DispatchQueue.main.async {
+//                        self.tableView.reloadData()
+//                    }
+//                }
+//                break
+//            }
+//        }
     }
     
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellIdentifier", for: indexPath)
+        cell.textLabel?.text = dataArray[indexPath.row].name
+        return cell
+    
+    }
+    
+    
+}
